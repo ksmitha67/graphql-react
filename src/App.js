c6c3cd8f3b6edcf5952ab52aa7ab8dda63eb4040
Kumari Smitha <ksmitha@deloitte.com>
2022-12-09 19:44:27 +0530

    feat: file upload and download code code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import { GraphQLEditor } from 'graphql-editor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FileSaver from 'file-saver';
+
+
+const types = {
+    name: `
+        type Query{
+            names: [Smita]
+        }
+    `,
+    nameLibrary: `
+        type Smita{
+            name: String;
+        }
+    `,
+};
+
+export const App = () => {
+
+const uploadRef = useRef(null);
+
+const [schema, setSchema] = useState({
+  code: types.name,
+  libraries: types.nameLibrary
+});
+
+const resetEditor = ()=>{
+    console.log(schema)
+    // setSchema(undefined)
+    console.log("code", schema.code)
+
+    setSchema({code: '', libraries:''});
+   
+    // console.log(schema)
 }
 
-export default App;
+const loadExisting = ()=>{
+    uploadRef.current.click();
+}
+
+const commitGraph =  ()=>{
+    var file = new File([schema.code], "hello world.graphqls", {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(file);
+
+}
+
+const handleData = (e)=>{
+    const data = e.target.result;
+    // console.log("file data", data);
+    setSchema({code : data, libraries : "",})
+}
+
+const handleChange = (file)=>{
+    let fileContent = new FileReader();
+    fileContent.onloadend  = handleData;
+    console.log(fileContent);
+    fileContent.readAsText(file);
+
+}
+
+return (
+    <>
+        <div className='main'>
+            <div className='buttons'>
+                <button onClick={resetEditor}>Create New Graph</button>
+                <button onClick={loadExisting}>Load Existing</button>
+                <button onClick={commitGraph}>Commit graph</button>
+                <input type="file" 
+                    style={{display:"none"}}
+                    ref = {uploadRef}
+                    onChange = {
+                        (e)=> handleChange(e.target.files[0])
+                    } 
+                />
+            </div>
+            <div className='editor'>
+        <GraphQLEditor
+            onSchemaChange={(props) => {
+            setSchema(props);
+            }}
+            schema={schema}
+        />
+        </div>
+        </div>
+    </>
+  );
+};
